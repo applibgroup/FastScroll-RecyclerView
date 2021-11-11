5329dde2dd53e50fb4f064b21c3e8d57646dc2e6
ShivamJamaiwar <shivam.jamaiwar@click2cloud.net>
2021-11-11 14:52:02 +0530

    Add HMOS code.

--- library/src/main/java/com/jaredrummler/fastscrollrecyclerview/FastScrollPopup.java
+++ library/src/main/java/com/jaredrummler/fastscrollrecyclerview/FastScrollPopup.java
@@ -18,7 +18,7 @@
 
 package com.jaredrummler.fastscrollrecyclerview;
 
-import android.animation.Animator;
+/*import android.animation.Animator;
 import android.animation.ObjectAnimator;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
@@ -28,8 +28,25 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
-import android.support.annotation.ColorInt;
 import android.util.AttributeSet;
+import androidx.annotation.ColorInt;*/
+
+import ohos.agp.animation.Animator;
+import ohos.agp.animation.AnimatorProperty;
+import ohos.agp.components.Attr;
+import ohos.agp.components.AttrSet;
+import ohos.agp.components.element.Element;
+import ohos.agp.components.element.VectorElement;
+import ohos.agp.render.Canvas;
+import ohos.agp.render.Paint;
+import ohos.agp.utils.Color;
+import ohos.agp.utils.Rect;
+import ohos.global.resource.NotExistException;
+import ohos.global.resource.ResourceManager;
+import ohos.global.resource.WrongTypeException;
+
+import java.io.IOException;
+import java.util.Optional;
 
 /**
  * The fast scroller popup that shows the section name the list will jump to.
@@ -38,40 +55,61 @@ public class FastScrollPopup {
 
   private static final float FAST_SCROLL_OVERLAY_Y_OFFSET_FACTOR = 1.5f;
 
+  private Color bgColor;
+  private Color textColor;
+  private float textSize;
+
   private final Rect backgroundBounds = new Rect(); // The absolute bounds of the fast scroller bg
   private final Rect invalidateRect = new Rect();
   private final Rect tmpRect = new Rect();
   private final Rect textBounds = new Rect();
 
   private FastScrollRecyclerView recyclerView;
-  private Animator alphaAnimator;
-  private Resources resources;
-  private Drawable background;
+  private AnimatorProperty alphaAnimator;
+  private ResourceManager resources;
+  private Element background;
   private Paint textPaint;
   private String sectionName;
   private int originalBackgroundSize;
   private float alpha;
   private boolean visible;
 
-  public FastScrollPopup(FastScrollRecyclerView rv, AttributeSet attrs) {
-    TypedArray ta = rv.getContext().obtainStyledAttributes(attrs, R.styleable.FastScrollRecyclerView);
+  public FastScrollPopup(FastScrollRecyclerView rv, AttrSet attrs) {
     recyclerView = rv;
     resources = rv.getResources();
-    int bgColor = ta.getColor(R.styleable.FastScrollRecyclerView_fastScrollPopupBackgroundColor, Color.TRANSPARENT);
-    int textColor = ta.getColor(R.styleable.FastScrollRecyclerView_fastScrollPopupTextColor, Color.WHITE);
-    float textSize = ta.getDimension(R.styleable.FastScrollRecyclerView_fastScrollTextSize, resources.getDimensionPixelSize(R.dimen.fastscroll_popup_text_size));
-    originalBackgroundSize = (int) textSize + (int) ta.getDimension(R.styleable.FastScrollRecyclerView_fastScrollPopupPadding, resources.getDimensionPixelSize(R.dimen.fastscroll_popup_default_padding));
-    background = resources.getDrawable(R.drawable.fastscroll_popup_bg);
-    if (bgColor != Color.TRANSPARENT) {
-      background = background.mutate();
-      background.setColorFilter(bgColor, PorterDuff.Mode.SRC_IN);
+
+    Optional<Attr> attr;
+    if (attrs != null) {
+      attr = attrs.getAttr(Attribute.FAST_SCROLL_POPUP_BACKGROUND_COLOR);
+      bgColor = attr.map(Attr::getColorValue).orElse(Color.TRANSPARENT);
+
+      attr = attrs.getAttr(Attribute.FAST_SCROLL_POPUP_TEXT_COLOR);
+      textColor = attr.map(Attr::getColorValue).orElse(Color.WHITE);
+
+      attr = attrs.getAttr(Attribute.FAST_SCROLL_TEXT_SIZE);
+      textSize = attr.map(Attr::getDimensionValue).orElse(ResourceTable.Float_fastscroll_popup_text_size);
+
+      attr = attrs.getAttr(Attribute.FAST_SCROLL_POPUP_PADDING);
+      originalBackgroundSize = (int) textSize + attr.map(Attr::getDimensionValue).orElse(ResourceTable.Float_fastscroll_popup_default_padding);
     }
+
+    try {
+      background = new VectorElement(getContext(), ResourceTable.Graphic_fastscroll_popup_bg);
+    } catch (IOException | NotExistException | WrongTypeException e) {
+      e.printStackTrace();
+    }
+
+    if (bgColor != Color.TRANSPARENT) {
+      /*background = background.mutate();
+      background.setColorFilter(bgColor, PorterDuff.Mode.SRC_IN);*/
+    }
+
     background.setBounds(0, 0, originalBackgroundSize, originalBackgroundSize);
     textPaint = new Paint();
     textPaint.setColor(textColor);
     textPaint.setAntiAlias(true);
-    textPaint.setTextSize(textSize);
-    ta.recycle();
+    textPaint.setTextSize((int) textSize);
+
   }
 
   /**
@@ -80,7 +118,7 @@ public class FastScrollPopup {
   protected void setSectionName(String sectionName) {
     if (!sectionName.equals(this.sectionName)) {
       this.sectionName = sectionName;
-      textPaint.getTextBounds(sectionName, 0, sectionName.length(), textBounds);
+      textPaint.getTextBounds(sectionName);
       // Update the width to use measureText since that is more accurate
       textBounds.right = (int) (textBounds.left + textPaint.measureText(sectionName));
     }
@@ -92,14 +130,14 @@ public class FastScrollPopup {
    * @return the invalidation rect for this update.
    */
   protected Rect updateFastScrollerBounds(FastScrollRecyclerView rv, int lastTouchY) {
-    invalidateRect.set(backgroundBounds);
+    invalidateRect.set(backgroundBounds.left, backgroundBounds.top, backgroundBounds.right, backgroundBounds.bottom);
 
     if (isVisible()) {
       // Calculate the dimensions and position of the fast scroller popup
       int edgePadding = rv.getMaxScrollbarWidth();
-      int bgPadding = (originalBackgroundSize - textBounds.height()) / 2;
+      int bgPadding = (originalBackgroundSize - textBounds.getHeight()) / 2;
       int bgHeight = originalBackgroundSize;
-      int bgWidth = Math.max(originalBackgroundSize, textBounds.width() + (2 * bgPadding));
+      int bgWidth = Math.max(originalBackgroundSize, textBounds.getWidth() + (2 * bgPadding));
       if (Utilities.isRtl(resources)) {
         backgroundBounds.left = rv.getBackgroundPadding().left + (2 * rv.getMaxScrollbarWidth());
         backgroundBounds.right = backgroundBounds.left + bgWidth;
@@ -112,11 +150,11 @@ public class FastScrollPopup {
           Math.max(edgePadding, Math.min(backgroundBounds.top, rv.getHeight() - edgePadding - bgHeight));
       backgroundBounds.bottom = backgroundBounds.top + bgHeight;
     } else {
-      backgroundBounds.setEmpty();
+      backgroundBounds.set(0,0,0,0);
     }
 
     // Combine the old and new fast scroller bounds to create the full invalidate rect
-    invalidateRect.union(backgroundBounds);
+    invalidateRect.fuse(backgroundBounds);
     return invalidateRect;
   }
 
@@ -129,7 +167,8 @@ public class FastScrollPopup {
       if (alphaAnimator != null) {
         alphaAnimator.cancel();
       }
-      alphaAnimator = ObjectAnimator.ofFloat(this, "alpha", visible ? 1f : 0f);
+      alphaAnimator = new AnimatorProperty();
+      alphaAnimator.alpha(visible ? 1f : 0f);
       alphaAnimator.setDuration(visible ? 200 : 150);
       alphaAnimator.start();
     }
@@ -138,19 +177,19 @@ public class FastScrollPopup {
   // Setter/getter for the popup alpha for animations
   public void setAlpha(float alpha) {
     this.alpha = alpha;
-    recyclerView.invalidate(backgroundBounds);
+    recyclerView.invalidate();
   }
 
   public float getAlpha() {
     return alpha;
   }
 
-  public void setBackgroundColor(@ColorInt int color) {
+  public void setBackgroundColor(Color color) {
     background = background.mutate();
     background.setColorFilter(color, PorterDuff.Mode.SRC_IN);
   }
 
-  public void setTextColor(@ColorInt int color) {
+  public void setTextColor(Color color) {
     textPaint.setColor(color);
   }
 
@@ -161,17 +200,19 @@ public class FastScrollPopup {
   protected void draw(Canvas c) {
     if (isVisible()) {
       // Draw the fast scroller popup
-      int restoreCount = c.save(Canvas.MATRIX_SAVE_FLAG);
+      int restoreCount = c.save();
       c.translate(backgroundBounds.left, backgroundBounds.top);
-      tmpRect.set(backgroundBounds);
-      tmpRect.offsetTo(0, 0);
+      tmpRect.set(backgroundBounds.left, backgroundBounds.top, backgroundBounds.right, backgroundBounds.bottom);
+      tmpRect.offset(0, 0);
       background.setBounds(tmpRect);
       background.setAlpha((int) (alpha * 255));
-      background.draw(c);
+      background.drawToCanvas(c);
       textPaint.setAlpha((int) (alpha * 255));
-      c.drawText(sectionName, (backgroundBounds.width() - textBounds.width()) / 2,
-          backgroundBounds.height() - (backgroundBounds.height() - textBounds.height()) / 2,
-          textPaint);
+
+      c.drawText(textPaint, sectionName,
+              (backgroundBounds.getWidth() - textBounds.getWidth()) / 2,
+              backgroundBounds.getHeight() - (backgroundBounds.getHeight() - textBounds.getHeight()) / 2);
+
       c.restoreToCount(restoreCount);
     }
   }
--- library/src/main/java/com/jaredrummler/fastscrollrecyclerview/Utilities.java
+++ library/src/main/java/com/jaredrummler/fastscrollrecyclerview/Utilities.java
@@ -17,15 +17,66 @@
 
 package com.jaredrummler.fastscrollrecyclerview;
 
-import android.content.res.Resources;
-import android.os.Build;
-import android.view.View;
+import ohos.agp.colors.RgbColor;
+import ohos.global.resource.ResourceManager;
 
 final class Utilities {
 
-  static boolean isRtl(Resources res) {
+  /*static boolean isRtl(Resources res) {
     return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1 &&
         res.getConfiguration().getLayoutDirection() == View.LAYOUT_DIRECTION_RTL;
+  }*/
+
+  static boolean isRtl(ResourceManager res) {
+    return res.getConfiguration().isLayoutRTL;
+  }
+
+  /**
+   * get the animated value with fraction and values
+   *
+   * @param fraction 0~1
+   * @param colors color array
+   * @return int color
+   */
+  public static int getAnimatedColor(float fraction, int... colors){
+    if(colors == null || colors.length == 0){
+      return 0;
+    }
+    if(colors.length == 1){
+      return getAnimatedColor(0,colors[0],fraction);
+    }else{
+      if(fraction == 1){
+        return colors[colors.length-1];
+      }
+      float oneFraction = 1f / (colors.length - 1);
+      float offFraction = 0;
+      for (int i = 0; i < colors.length - 1; i++) {
+        if (offFraction + oneFraction >= fraction) {
+          return getAnimatedColor(colors[i], colors[i + 1], (fraction - offFraction) * (colors.length -1));
+        }
+        offFraction += oneFraction;
+      }
+    }
+    return 0;
+  }
+
+  /**
+   * get the animated color with start color, end color and fraction
+   *
+   * @param fraction 0~1
+   * @param from start color
+   * @param to end color
+   * @return int color
+   */
+  public static int getAnimatedColor(int from, int to, float fraction) {
+    RgbColor colorFrom = RgbColor.fromArgbInt(from);
+    RgbColor colorTo = RgbColor.fromArgbInt(to);
+    int red = (int) (colorFrom.getRed() + (colorTo.getRed() - colorFrom.getRed()) * fraction);
+    int blue = (int) (colorFrom.getBlue() + (colorTo.getBlue() - colorFrom.getBlue()) * fraction);
+    int green = (int) (colorFrom.getGreen() + (colorTo.getGreen() - colorFrom.getGreen()) * fraction);
+    int alpha = (int) (colorFrom.getAlpha() + (colorTo.getAlpha() - colorFrom.getAlpha()) * fraction);
+    RgbColor mCurrentColorRgb = new RgbColor(red, green, blue, alpha);
+    return mCurrentColorRgb.asArgbInt();
   }
 
 }
